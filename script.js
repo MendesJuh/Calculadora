76dc730edae3e70008156edd354475bf59bbc291
Juliana Mendes <juliana-dantas@live.com>
2022-06-24 15:02:41 -0300

    calculadora

---
+++ script.js
@@ -0,0 +1,22 @@
+
+function insert(num) {
+    const number = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = number + num;
+}
+
+function clean() {
+    document.getElementById('result').innerHTML = "";
+}
+function back() {
+    let result = document.getElementById('result').innerHTML;
+    document.getElementById('result').innerHTML = result.substring(0, result.length - 1);
+}
+function calcular() {
+    let result = document.getElementById('result').innerHTML;
+    if (result) {
+        document.getElementById('result').innerHTML = eval(result);
+    }
+    else {
+        document.getElementById('result').innerHTML = ""
+    }
+}
